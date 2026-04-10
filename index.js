7806c59fa70e92f350cc5e3d05046475f6253693
vipersmp59-ship-it <vipersmp59@gmail.com>
2026-04-10 17:27:01 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const mineflayer = require('mineflayer');
+
+function createBot() {
+    const bot = mineflayer.createBot({
+        host: 'Viper-SMP.aternos.me:62227', // <-- ТВОЙ АДРЕС ТУТ
+        port: 25565,
+        username: 'AFK_Bot_Pro', // Ник бота
+        version: false // Авто-определение версии
+    });
+
+    bot.on('spawn', () => {
+        console.log('Бот в игре!');
+        // Бот будет прыгать каждые 15 секунд
+        setInterval(() => {
+            bot.setControlState('jump', true);
+            setTimeout(() => bot.setControlState('jump', false), 500);
+        }, 15000);
+    });
+
+    // Если выкинуло — перезаходим через 30 секунд
+    bot.on('end', () => {
+        console.log('Бот вылетел, переподключаюсь...');
+        setTimeout(createBot, 30000);
+    });
+
+    bot.on('error', (err) => console.log('Ошибка:', err));
+}
+
+createBot();
